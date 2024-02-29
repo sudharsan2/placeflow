d9ba427d007bf3e48892be518a571c8ca9486d9f
sudharsan <sudharsanselvam2@gmail.com>
2024-02-29 17:34:48 +0530

    Send Registration Template2

--- CIR/views.py
+++ CIR/views.py
@@ -3,16 +3,19 @@ from rest_framework.views import APIView
 from .serializers import postcompanyDataSerializer,getcompanyDataSerializer,excelRegistrationSerializer,excelAddStudentInfoSerializer
 from tablemanagement.models import companyData,studentData
 from rest_framework.response import Response
+from django.http import FileResponse
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 import base64
-from django.http import HttpRequest
+from django.http import HttpRequest,HttpResponse
 import pandas as pd
 import requests
 from io import BytesIO
 from django.conf import settings
 import base64
-
+import openpyxl
+import os
+# from django.http import HttpResponse
 
 
 class enterCompanyDataAPI(APIView):
@@ -108,22 +111,40 @@ class handleExcelStudentInfo(APIView):
 class sendRegisterationTemplateAPI(APIView):
     def get(self, request):
         file_path =  (settings.BASE_DIR / "ExcelTemplate/Book10.xlsx").resolve()
-        with open(file_path,'rb') as file:
-            file_content = file.read()
+        # with open(file_path,'rb') as file:
+        #     file_content = file.read()
 
-        base64_content = base64.b64encode(file_content).decode('utf-8')
-        meta_data = {
-            "file_name" : "Registeration Template",
-            "file_size" : len(file_content)
-        }
+        # # base64_content = base64.b64encode(file_content).decode('utf-8')
+        # meta_data = {
+        #     "file_name" : "Registeration Template",
+        #     "file_size" : len(file_content)
+        # }
 
-        response_data = {
-            # "metadata" : meta_data,
-            "Template" : base64_content
-        }
+        # response_data = {
+        #     # "metadata" : meta_data,
+        #     "Template" : file_content
+        # }
 
-        return Response(response_data,status=status.HTTP_200_OK)
+        # return FileResponse(file_content, as_attachment=True, filename="Registration_Template.xlsx")
+        # df = pd.read_excel(file_path)
 
+        #         # Convert DataFrame to blob
+        # excel_data = BytesIO()
+        # with pd.ExcelWriter(excel_data, engine='xlsxwriter') as writer:
+        #     df.to_excel(writer, index=False)
+        # excel_data.seek(0)
+        # Return the blob as response
+
+        with open(file_path,'rb') as excel_file:            
+# Create BytesIO object
+            excel_bytes = BytesIO(excel_file.read())        
+# Create response
+        response = HttpResponse(excel_bytes, content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+        response['Content-Disposition'] ='attachment; filename="registerTemplate.xlsx"'
+        return response
+        
+        
+        
 
         
 
